f403fbf5ea7f5163e94c6f99374cdecd31f0522e
Łukasz Domagalski <lukasz.domagalski@aidadx.io>
2020-11-21 15:50:03 +0100

    Dodaj pliki projektów.

---
+++ Scribe.Studio.Logic/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scribe.Studio.Logic
+{
+    public static class Configuration
+    {
+        public static List<KeyValuePair<string, object>> Parameters;
+
+        public static void SaveConfiguration(string fileName)
+        {
+            string fileContent = "";
+            //foreach(KeyValuePair<string, object> parameter in Parameters)
+            //{
+            //    fileContent += JsonConvert.SerializeObject(Para);
+            //}
+            StreamWriter sw = new StreamWriter(fileName);
+            sw.WriteLine(fileContent);
+            sw.Close();
+        }
+
+        public static void LoadConfiguration(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Scribe.Studio.Logic/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scribe.Studio.Logic
+{
+    public class Database
+    {
+    }
+}
