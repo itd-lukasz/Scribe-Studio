9dc4cd4c7ecc853e57ca9aa14f2dcf1500631677
itd-lukasz <lukasz.domagalski@itdoors.pl>
2020-11-26 13:12:31 +0100

    Queue Send Message

---
+++ Scribe.Studio.Logic/Queue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+
+namespace Scribe.Studio.Logic
+{
+    public static class Queue
+    {
+        public static void SendMessage(string path, string label, string content)
+        {
+            using (MessageQueue queue = new MessageQueue(path))
+            {
+                queue.MessageReadPropertyFilter.AppSpecific = true;
+                queue.Formatter = new BinaryMessageFormatter();
+                Message myMessage = new Message();
+                myMessage.Label = label;
+                myMessage.BodyStream = new MemoryStream(Encoding.ASCII.GetBytes(content));
+                queue.Send(myMessage);
+            }
+        }
+    }
+}
